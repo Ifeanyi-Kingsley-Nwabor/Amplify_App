8d7af55f33ddcfdb05e63f74be78c12bdfbece9e
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2022-02-14 23:37:05 +0100

    add crud operations and s3 storage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API, Storage } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { listNotes } from "../graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "../graphql/mutations";
+
+const initialFormState = { name: "", description: "" };
 
 const Dashboard = () => {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return;
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
+  }
+
+  const what = "Note";
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const image = await Storage.get(note.image);
+          note.image = image;
+        }
+        return note;
+      })
+    );
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: formData },
+    });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([...notes, formData]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter((note) => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
+
   return (
-    <div>
+    <div className="App">
       <Authenticator>
         {({ signOut, user }) => (
           <div>
@@ -13,6 +75,39 @@ const Dashboard = () => {
           </div>
         )}
       </Authenticator>
+      <div>
+        <h1>My {what} App</h1>
+        <input type="file" onChange={onChange} />
+        <input
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          placeholder="Note Name"
+          value={formData.name}
+        />
+        <input
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          placeholder="Note Description"
+          value={formData.description}
+        />
+        <button onClick={createNote}>New Note</button>
+        <div style={{ marginBottom: 30 }}>
+          {notes.map((note) => (
+            <div key={note.id || note.name}>
+              {note.image && (
+                <img
+                  src={note.image}
+                  style={{ width: 400 }}
+                  alt="this is a representation of the subject note."
+                />
+              )}
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete Note</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Home = () => {
+  const what = "Note";
   return (
     <div>
       <h1>Welcome to Zombeatz Home page!</h1>
-      <h4>Find the songs of your favorite artists.</h4>
+      <h4>Find the {what} of your favorite artists.</h4>
       <h6>
-        Are you an Artist, a producer or you have a song you would like to go
-        global, create a new account and upload your songs for free.
+        Are you an Artist, a producer or you have a {what} you would like to go
+        global, create a new account and upload your {what} for free.
       </h6>
     </div>
   );
